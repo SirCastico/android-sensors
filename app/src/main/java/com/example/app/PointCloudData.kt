824e0ae2c858e9846c28f9150201eb7f6c97c950
sircastico <carlosemanuelfb2@gmail.com>
2024-11-17 18:57:20 +0000

    filter by arcore planes

--- app/src/main/java/com/example/app/PointCloudData.kt
+++ app/src/main/java/com/example/app/PointCloudData.kt
@@ -1,13 +1,15 @@
 package com.example.app
 
 import com.example.app.PointCloudHelper.convertDepthTo3dCameraSpacePointBuffer
-import com.example.app.PointCloudHelper.convertDepthTo3dWorldSpacePointBuffer
 import com.google.ar.core.Anchor
 import com.google.ar.core.Frame
+import com.google.ar.core.Plane
 import com.google.ar.core.Pose
 import com.google.ar.core.Session
+import com.google.ar.core.TrackingState
 import com.google.ar.core.exceptions.NotYetAvailableException
 import java.nio.FloatBuffer
+import kotlin.math.abs
 
 
 class PointCloudData(
@@ -30,6 +32,9 @@ class PointCloudData(
                     depthImage, confidenceImage, intrinsics, pointLimit
                 )
 
+                //filterUsingPlanes(points, session.getAllTrackables())
+                filterUsingPlanes(points, session.getAllTrackables(Plane::class.java))
+
                 depthImage.close()
                 confidenceImage.close()
 
@@ -43,6 +48,49 @@ class PointCloudData(
                 // spam the logcat with this.
             }
             return null
+        }
+    }
+}
+
+
+fun filterUsingPlanes(points: FloatBuffer, allPlanes: Collection<Plane>) {
+    val planeNormal = FloatArray(3)
+
+    // Allocate the output buffer.
+    val numPoints: Int = points.remaining() / PointCloudData.VALUES_PER_POINT
+
+    // Check each plane against each point.
+    for (plane in allPlanes) {
+        if (plane.trackingState !== TrackingState.TRACKING || plane.getSubsumedBy() != null) {
+            continue
+        }
+
+        // Compute the normal vector of the plane.
+        val planePose: Pose = plane.getCenterPose()
+        planePose.getTransformedAxis(1, 1.0f, planeNormal, 0)
+
+        // Filter points that are too close to the plane.
+        for (index in 0 until numPoints) {
+            // Retrieves the next point.
+            val x: Float = points.get(PointCloudData.VALUES_PER_POINT * index)
+            val y: Float = points.get(PointCloudData.VALUES_PER_POINT * index + 1)
+            val z: Float = points.get(PointCloudData.VALUES_PER_POINT * index + 2)
+
+            // Transform point to be in world coordinates, to match plane info.
+            val distance =
+                ((x - planePose.tx()) * planeNormal[0] + (y - planePose.ty()) * planeNormal[1] + (z - planePose.tz()) * planeNormal[2])
+            // Controls the size of objects detected.
+            // Smaller values mean smaller objects will be kept.
+            // Larger values will only allow detection of larger objects, but also helps reduce noise.
+            if (abs(distance.toDouble()) > 0.03) {
+                continue  // Keep this point, since it's far enough away from the plane.
+            }
+
+            // Invalidate points that are too close to planar surfaces.
+            points.put(PointCloudData.VALUES_PER_POINT * index, 0.0f)
+            points.put(PointCloudData.VALUES_PER_POINT * index + 1, 0.0f)
+            points.put(PointCloudData.VALUES_PER_POINT * index + 2, 0.0f)
+            points.put(PointCloudData.VALUES_PER_POINT * index + 3, 0.0f)
         }
     }
 }
